91bb99e8933635abd885bdaa5f6af504410a7e5c
Rachel Bressner <rachel.bressner@gmail.com>
2018-03-20 15:12:49 -0400

    plus sign buttons

--- index.html/main.js
+++ index.html/main.js
@@ -6,3 +6,12 @@ function paperInfoButton() {
     paper.style.visibility = "visible"
   }
 }
+
+function paperInfoDropDown() {
+  let paper = document.getElementById("plus-sign")
+  if (paper.style.visibility === "visible") {
+    paper.style.visibility = "hidden"
+  } else {
+    paper.style.visibility = "visible"
+  }
+}
